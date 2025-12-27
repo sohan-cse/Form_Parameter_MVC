45f2f5199c86549e97760c87f28acac48a3c67f5
Md. Ibn Masud <sohan.cse.pust@gmail.com>
2025-12-27 22:49:21 +0600

    Add project files.

---
+++ Form_Parameter_MVC_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Form_Parameter_MVC_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
