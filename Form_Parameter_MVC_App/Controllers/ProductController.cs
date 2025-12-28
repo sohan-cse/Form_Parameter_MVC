05c45003494ebd35fad8b08cbacffa135a5121d7
Md. Ibn Masud <sohan.cse.pust@gmail.com>
2025-12-28 12:30:25 +0600

    Form Collection Done on Product

---
+++ Form_Parameter_MVC_App/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Form_Parameter_MVC_App.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        //Passing Data using Form Collection to view
+        public IActionResult Index(IFormCollection data)
+        {
+            ViewBag.pid = data["productId"];
+            ViewBag.pname = data["productName"];
+            ViewBag.pprice = data["productPrice"];
+            return View();
+        }
+        public IActionResult ProductForm()
+        {
+            return View();
+        }
+    }
+}
