42b6f6cf288e65ac86f593746c6925473888d472
Md. Ibn Masud <sohan.cse.pust@gmail.com>
2025-12-28 13:56:46 +0600

    Model object on Course done

---
+++ Form_Parameter_MVC_App/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using Form_Parameter_MVC_App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Form_Parameter_MVC_App.Controllers
+{
+    public class CourseController : Controller
+    {
+        //Passing Data using Model Binding to view
+        [HttpPost]
+        public IActionResult Index(Course course)
+        {
+            ViewBag.cid = course.CourseId;
+            ViewBag.cname = course.CourseName;
+            ViewBag.ccredit = course.CourseCredit;
+            ViewBag.grade = course.Grade;
+            return View();
+        }
+        public IActionResult CourseForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ Form_Parameter_MVC_App/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace Form_Parameter_MVC_App.Models
+{
+    public class Course
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal CourseCredit { get; set; }
+        public decimal Grade { get; set; }
+
+    }
+}
